4f4d286914124bb5f252e149a49831dda7ed1f4d
Gary Patricelli <garypatricelli@gmail.com>
2014-02-25 23:01:44 -0500

    add basic board printing

--- sodoku.scala
+++ sodoku.scala
@@ -3,15 +3,15 @@ import scala.io.Source
 
 
 // Function to initialize sodoku from file
-def initBoard(filename: String) : Array[Array[Int]] = {
+def initBoard(filename: String) : Array[Array[Char]] = {
   
   // Open file
   var lineNum = 0
-  var board = ofDim[Int](0,0)
+  var board = ofDim[Char](0,0)
   for (line <- Source.fromFile(filename).getLines()) {
       // Init board if first line 
       if (lineNum == 0) {
-        board = ofDim[Int](line.length, line.length)
+        board = ofDim[Char](line.length, line.length)
         println("Initializing Board...")
       }
       //Parse this line of the file
@@ -23,15 +23,14 @@ def initBoard(filename: String) : Array[Array[Int]] = {
 }
 
 // Function to parse a single line from a file into the board
-def parseInput(lineNum: Int, line: String,  board: Array[Array[Int]]){
+def parseInput(lineNum: Int, line: String,  board: Array[Array[Char]]){
   var index = 0
   while (index < line.length) {
     if (line.charAt(index) == '.') {
       // This is a blank space, we denote it with -1
-      board(lineNum)(index) = -1 
+      board(lineNum)(index) = 'x' 
     }
     else {
-      println(line.charAt(index))
       board(lineNum)(index) = (line.charAt(index))
     }
     index = index + 1
@@ -39,12 +38,12 @@ def parseInput(lineNum: Int, line: String,  board: Array[Array[Int]]){
 }
 
 // Function to print board to standard out
-def printBoard(board: Array[Array[Int]]) {
+def printBoard(board: Array[Array[Char]]) {
   var rowIndex = 0
   while (rowIndex < board.length) {
     var columnIndex = 0
     while (columnIndex < board.length) {
-      if (board(rowIndex)(columnIndex) == -1){
+      if (board(rowIndex)(columnIndex) == 'x'){
         print(" ")
       }
       else {
